a7531a80293c5276812d88b8b8a49158a3a8069e
Taiwo Obadare <taiwo.obadare@gmail.com>
2020-11-14 18:13:04 +0000

    Initial Commit

--- photographerproject/settings.py
+++ photographerproject/settings.py
@@ -141,5 +141,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-DEFAULT_FROM_EMAIL = 'x00179223@mytudublin.ie'
+DEFAULT_FROM_EMAIL = 'taiwo.obadare@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
